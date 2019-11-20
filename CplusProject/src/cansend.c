a5f3842f97e2e7c102fe3268e1dbf6a286bdcb94
Daniel Garcia <danigarciape@gmail.com>
2019-11-20 20:52:51 +0100

    Código de Can

--- CplusProject/src/cansend.c
+++ CplusProject/src/cansend.c
@@ -1,112 +1,57 @@
-/* SPDX-License-Identifier: (GPL-2.0-only OR BSD-3-Clause) */
-/*
- * cansend.c - simple command line tool to send CAN-frames via CAN_RAW sockets
- *
- * Copyright (c) 2002-2007 Volkswagen Group Electronic Research
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 3. Neither the name of Volkswagen nor the names of its contributors
- *    may be used to endorse or promote products derived from this software
- *    without specific prior written permission.
- *
- * Alternatively, provided that this notice is retained in full, this
- * software may be distributed under the terms of the GNU General
- * Public License ("GPL") version 2, in which case the provisions of the
- * GPL apply INSTEAD OF those given above.
- *
- * The provided data structures and external interfaces from this code
- * are not restricted to be used by modules with a GPL compatible license.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
- * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
- * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
- * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
- * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
- * DAMAGE.
- *
- * Send feedback to <linux-can@vger.kernel.org>
- *
- */
+#include "candsend.h"
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-
-#include <net/if.h>
-#include <sys/ioctl.h>
-#include <sys/socket.h>
-
-#include <linux/can.h>
-#include <linux/can/raw.h>
-
-#include "lib.h"
-
-int main()
+Ccan::Ccan(sockaddr_can Can_Address)
 {
-	int s; /* can raw socket */ 
-	int required_mtu;
-	int mtu;
-	int enable_canfd = 1;
-	struct sockaddr_can addr;
-	struct canfd_frame frame;
-	struct ifreq ifr;
-
+	system("sudo /sbin/ip link set can0 up type can bitrate 500000");
+	addr = Can_Address;
 	/* open socket */
-	if ((s = socket(PF_CAN, SOCK_RAW, CAN_RAW)) < 0) {
-		perror("socket");
-		return 1;
+	if ((s = socket(PF_CAN, SOCK_RAW, CAN_RAW)) < 0)
+	{
+		throw("Error opening socket");
 	}
-
-	strncpy(ifr.ifr_name, argv[1], IFNAMSIZ - 1);
+	strncpy(ifr.ifr_name, addr, IFNAMSIZ - 1);
 	ifr.ifr_name[IFNAMSIZ - 1] = '\0';
 	ifr.ifr_ifindex = if_nametoindex(ifr.ifr_name);
-	if (!ifr.ifr_ifindex) {
-		perror("if_nametoindex");
-		return 1;
+	if (!ifr.ifr_ifindex)
+	{
+		throw("Error in if_nametoindex");
 	}
 
 	memset(&addr, 0, sizeof(addr));
 	addr.can_family = AF_CAN;
 	addr.can_ifindex = ifr.ifr_ifindex;
+}
 
+candsend::~candsend()
+{
+	close(s);
+}
 
-		/* ensure discrete CAN FD length values 0..8, 12, 16, 20, 24, 32, 64 */
-		frame.len = can_dlc2len(can_len2dlc(frame.len));
-	}
+void SetFrame(int ID, int *data){
+	frame.can_id = ID;
+	frame.data[0] = data[0];
+	frame.data[1] = data[1];
+}
 
+void CCan::Write(int id, int* data)
+{	
+	SetFrame(id,data);
+	/* ensure discrete CAN FD length values 0..8, 12, 16, 20, 24, 32, 64 */
+	frame.len = can_dlc2len(can_len2dlc(frame.len));
 	/* disable default receive filter on this RAW socket */
 	/* This is obsolete as we do not read from the socket at all, but for */
 	/* this reason we can remove the receive list in the Kernel to save a */
 	/* little (really a very little!) CPU usage.                          */
 	setsockopt(s, SOL_CAN_RAW, CAN_RAW_FILTER, NULL, 0);
 
-	if (bind(s, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
-		perror("bind");
-		return 1;
+	if (bind(s, (struct sockaddr *)&addr, sizeof(addr)) < 0)
+	{
+		throw("Error in binding");
 	}
-
 	/* send frame */
-	if (write(s, &frame, required_mtu) != required_mtu) {
-		perror("write");
-		return 1;
+	if (write(s, &frame, required_mtu) != required_mtu)
+	{
+		throw("Error in the writing");
 	}
-
-	close(s);
-
-	return 0;
 }
+
--- CplusProject/src/main.cpp
+++ CplusProject/src/main.cpp
@@ -1,5 +1,62 @@
 #include <iostream>
+#include <pthread.h>
+#include "candsend.h"
 
-int main(int argc, char *argv[]) {
-	std::cout << "Hello Easy C++ project!" << std::endl;
+#define TS 100E6 //100ms
+
+using namespace std;
+
+//-------------DECLARACIÓN DE VARIABLE GLOBAL
+uint16_t dato[2];
+uint16_t id;
+pthread_mutex_t cerrojo;
+Ccan Can;
+
+struct timespec operator+(struct timespec t1, struct timespec t2)
+{
+	struct timespec suma;
+	suma.tv_sec = t1.tv_sec + t2.tv_sec;	// calcula segundos
+	suma.tv_nsec = t1.tv_nsec + t2.tv_nsec; // calcula nsegundos
+	if (suma.tv_nsec >= 1E9)				// ver si tenemos un segundo completo
+	{
+		suma.tv_sec++;		 // añadir 1 a la cuenta de segundos
+		suma.tv_nsec -= 1E9; // calcular los ns que quedan
+	}
+	return suma;
+}
+
+void *HiloCan(void *)
+{
+	struct timespec inicio, final, siguiente, intervalo;
+	intervalo.tv_nsec = TS;
+	clock_gettime(CLOCK_REALTIME, &siguiente);
+	inicio = siguiente;
+	while (1)
+	{
+		siguiente = siguiente + intervalo;
+		pthread_mutex_lock(&cerrojo);
+		Can.Write(id, *dato);
+		pthread_mutex_unlock(&cerrojo);
+		clock_nanosleep(CLOCK_REALTIME, TIMER_ABSTIME, &siguiente, nullptr);
+	}
+}
+
+void *HiloI2c(void *)
+{
+	//TODO
+}
+
+int main()
+{
+
+	pthread_t h_can, h_i2c;
+
+	if (pthread_mutex_init(&cerrojo, NULL) == -1)
+		throw("Error al crear el mutex");
+
+	pthread_create(&h_can, NULL, HiloCan, 0);
+	pthread_create(&h_i2c, NULL, HiloI2c, 0);
+
+	pthread_join(h_can, nullptr);
+	pthread_join(h_i2c, nullptr);
 }
